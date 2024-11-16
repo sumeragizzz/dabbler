17674af505fe23786873c3796aa6cf163829f1a4
sumeragizzz <developer@sumeragizzz.dev>
2024-11-16 10:02:21 +0900

    Create project by Spring Initializr

---
+++ src/main/java/dev/sumeragizzz/dabbler/DabblerApplication.java
@@ -0,0 +1,13 @@
+package dev.sumeragizzz.dabbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabblerApplication.class, args);
+	}
+
+}
